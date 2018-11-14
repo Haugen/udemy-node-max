5e32f57e79848cb1194df635850b73122cf3fbd3
Tobias Haugen <hej@tobiashaugen.se>
2018-11-14 16:38:48 -0300

    Start using Express, and first example of thinking behind middleware.

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const http = require('http');
-const routes = require('./routes');
 
-const server = http.createServer(routes);
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('In middleware 1');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('In middleware 2');
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('In middleware 3');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000, 'localhost');
