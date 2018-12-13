17c54afee6d01d41c69aaa10cfe7eb32c123347e
Tobias Haugen <hej@tobiashaugen.se>
2018-12-13 18:09:33 -0500

    Starting with building a REST api

--- util/errorHandling.js
+++
@@ -1,8 +0,0 @@
-const errorHandling = error => {
-  console.log(error);
-  const err = new Error(error);
-  err.httpStatusCode = 500;
-  return err;
-};
-
-module.exports = errorHandling;
--- util/path.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-// Helper function. rootDir will give the absolute dir path for wherever it's used.
-const rootDir = path.dirname(process.mainModule.filename);
-
-module.exports = rootDir;
