3cdc0fb36295729a86b2aab05831c081b652d2c7
Tobias Haugen <hej@tobiashaugen.se>
2018-12-19 11:38:32 -0500

    Cleaning up posts array after deleteing post.

--- controllers/feed.js
+++ controllers/feed.js
@@ -154,7 +154,14 @@ exports.deletePost = (req, res, next) => {
       }
       return Post.findByIdAndRemove(postId);
     })
-    .then(result => {
+    .then(() => {
+      return User.findById(req.userId);
+    })
+    .then(user => {
+      user.posts.pull(postId);
+      return user.save();
+    })
+    .then(() => {
       res.status(200).json({
         message: 'Post successfully deleted.',
         post: result
